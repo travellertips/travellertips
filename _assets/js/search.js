746111f2c7cedd4c3022a083c827847ca81d2089
Grygorii Polinovskyi <polinovskyi.g@gmail.com>
2018-03-29 17:10:18 +0200

    Search results styling

--- _assets/js/search.js
+++ _assets/js/search.js
@@ -9,8 +9,8 @@
       for (var i = 0; i < results.length; i++) {  // Iterate over the results
         var item = store[results[i].ref];
           
-        appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
-        appendString += '<p>' + item.summary.substring(0, 150) + '...</p></li>';
+        appendString += '<li><h3><a href="' + item.url + '">' + item.title + '</a></h3>';
+        appendString += '<div class="post-summary"><a href="' + item.url + '">' + item.thumbsmall + '</a><p>' + item.summary.substring(0, 200) + '...</p></div></li>';
       }
 
       searchResults.innerHTML = appendString;
