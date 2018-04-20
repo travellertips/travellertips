d6258861273c252805ebb2ecaebc4e0b42b62ac7
Grygorii Polinovskyi <polinovskyi.g@gmail.com>
2018-04-20 12:21:33 +0200

    UI fixes, adding mobile images for main page

--- _assets/js/search.js
+++ _assets/js/search.js
@@ -14,7 +14,7 @@
       for (var i = 0; i < results.length; i++) {  // Iterate over the results
         var item = store[results[i].ref];
           
-        appendString += '<li><h3><a href="' + item.url + '">' + item.title + '</a></h3>';
+        appendString += '<li><h2><a href="' + item.url + '">' + item.title + '</a></h2>';
         appendString += '<div class="post-summary"><a href="' + item.url + '">' + item.thumbsmall + '</a><p>' + item.summary.substring(0, 200) + '...</p></div></li>';
       }
 
