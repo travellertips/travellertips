916ade262f8a17f952dd2bb0483eb1166b286ada
Grygorii Polinovskyi <polinovskyi.g@gmail.com>
2018-07-20 15:07:09 +0200

    Release 2, additions

--- Gemfile
+++ Gemfile
@@ -6,13 +6,12 @@ gem "sass"
 # If you have any plugins, put them here!
 group :jekyll_plugins do
     gem "jekyll-feed", "~> 0.6"
-#    gem "autoprefixer-rails"
     gem "html-proofer"
     gem "jekyll-assets"
     gem "jekyll-sitemap"
-#    gem "mini_magick"
     gem "uglifier"
     gem "jekyll-paginate"
+    #    gem "mini_magick"
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- run_prod.sh
+++ run_prod.sh
@@ -1,4 +1,21 @@
 #bash
 
 JEKYLL_ENV=production bundle exec jekyll build --config _config_prod.yml
-jekyll serve
+#jekyll serve
+
+cd _site
+rm Gemfile.lock
+rm manifest.json
+rm package-lock.json
+rm new_img_list.txt
+rm new_maps_list.txt
+rm old_img_list.txt
+rm old_maps_list.txt
+rm proceed_list.txt
+rm proceed_maps_list.txt
+rm all_images_process.sh
+rm new_images_process.sh
+rm new_maps_process.sh
+rm run_prod.sh
+rm run.sh
+rm Gemfile
